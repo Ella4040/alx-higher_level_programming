bd90a53dfe1d2790bd4a8094daa5de814ce9f8f7
Ella4040 <usouella@gmail.com>
2023-08-30 16:02:44 -0700

    udapte

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Create a square class"""
+
+
 class Square:
 
+    """Create a square object"""
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
+
+        """Use __init__ to initialize square with private size.
+        Args:
+             size (int): Private size of square.
+        """
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+
         if size < 0:
-            raise ValueError("size must be >= 0") 
+            raise ValueError("size must be >= 0")
 
         self.__size = size
 
+    def area(self):
+        """Return area of the square
+        Args:
+            size (int): size of square
+        """
 
-        self.__size
-def area(self):
-        return self.__size ** 2
+        return (self.__size * self.__size)
