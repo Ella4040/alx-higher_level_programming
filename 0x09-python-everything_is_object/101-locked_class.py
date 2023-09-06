42a2534aff0a114d6416f6b396c131d67d6c5dee
Ella4040 <usouella@gmail.com>
2023-09-05 19:58:47 -0700

    udapte

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,10 +4,18 @@ This is a module that containts a clas that avoids
 dynmaically created attributes
 """
 
-
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    Secret class
+    """
 
-    def __init__(self):
-        """ Init method """
-        pass
+    def __setattr__(self, attr, value):
+        """
+        Method that allows the creation of first_name attribute but prevents
+        the creation of any other instance attributes.
+        """
+        msg = "'LockedClass' object has no attribute"
+        if attr == 'first_name':
+            super().__setattr__(attr, value)
+        else:
+            raise AttributeError(msg+" '{}'".format(attr))
