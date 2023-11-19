56af4a6a21233a95eca0ed4744fa29efd9020e83
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-11-19 15:58:37 -0800

    Update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Displays all values in the states table of the database hbtn_0e_0_usa
-whose name matches that supplied as argument.
-Safe from SQL injections.
-Usage: ./3-my_safe_filter_states.py <mysql username> \
-                                    <mysql password> \
-                                    <database name> \
-                                    <state name searched>
+"""A script that takes in an argument and displays 
+    all values in the states table of hbtn_0e_0_usa
+     My script has 3 arguments :  mysql username
+                                  mysql password
+                                  database name
+                                 state name searched
 """
 
 import sys
