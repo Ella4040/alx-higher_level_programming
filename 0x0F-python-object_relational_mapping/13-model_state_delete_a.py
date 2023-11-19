f0a469cfb7dc134363fbfb0f912de1b28d414482
Sourou Ella DOSSOU <125446466+Ella4040@users.noreply.github.com>
2023-11-19 15:29:28 -0800

    Update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa.
-Usage: ./13-model_state_delete_a.py <mysql username> /
-                                    <mysql password> /
-                                    <database name>
+"""A script that deletes all State objects with 
+   a name containing the letter a from 
+   the database hbtn_0e_6_usa
+   My script has 3 arguments: mysql username
+                              mysql password
+                              database name
 """
-
 
 import sys
 from sqlalchemy import create_engine
